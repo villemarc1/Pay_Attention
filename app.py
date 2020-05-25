bf7f5f2614a402ba792a0854ab259966bf3402bc
villemarc1 <47375397+villemarc1@users.noreply.github.com>
2020-05-25 08:51:50 -0300

    ate bootstrap local

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, request, redirect , url_for, render_template
+from flask import Flask, request, redirect , url_for, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+#app = Flask(__name__)
+app = Flask(__name__ , 
+template_folder="views",
+static_folder="public")
+app.config["SECRET_KEY"] = "secrete"
+
 #Bootstrap(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
@@ -17,8 +22,6 @@ class User(db.Model):
     password = db.Column(db.String(255), nullable=False)
     profile = db.relationship('Profile', backref='User', uselist=False)
 
-
-
     def __str__(self):
         return self.name
 
@@ -28,11 +31,69 @@ class Profile(db.Model):
     photo = db.Column(db.Unicode(124), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
+    def __str__(self):
+        return self.name
 
+class Disciplina(db.Model): 
+    __tablename__ = "Disciplina" 
+    id = db.Column(db.Integer, primary_key=True)
+    Disciplina = db.Column(db.String(255), nullable=False)
+    
+    def __str__(self):
+        return self.name
+
+class Disc_Assunto(db.Model): 
+    __tablename__ = "Disciplina_Assuntos" 
+    id = db.Column(db.Integer, primary_key=True)
+    Assunto = db.Column(db.String(124), nullable=False)
+    Disciplina_Id = db.Column(db.Integer, db.ForeignKey("Disciplina.id"))
 
     def __str__(self):
         return self.name
 
+class Exercicio(db.Model): 
+    __tablename__ = "Exercicios" 
+    Disciplina_Id = db.Column(db.Integer ,db.ForeignKey("Disciplina.id"))
+    Disciplina_Assunto_ID = db.Column(db.Integer, db.ForeignKey("Disciplina_Assuntos.id"))
+    Exe_Questoes_ID    = db.Column(db.Integer, primary_key=True)
+    Exe_Questoes_Desc  = db.Column(db.String(255) , nullable=False)
+    Exe_Questoes_Respostas    = db.Column(db.String(10) , nullable=False)
+    Exe_Questoes_Solução      = db.Column(db.String(255) , nullable=False)
+
+    def __str__(self):
+        return self.name
+
+class Exercicio(db.Model): 
+    __tablename__ = "Exercicios_Alternativas" 
+    Disciplina_Id = db.Column(db.Integer ,db.ForeignKey("Disciplina.id"))
+    Disciplina_Assunto_ID = db.Column(db.Integer, db.ForeignKey("Disciplina_Assuntos.id"))
+    Exe_Questoes_ID    = db.Column(db.Integer , db.ForeignKey("Exercicios.Exe_Questoes_ID"))
+    Exe_Questoes_Alternativas_ID = db.Column(db.Integer, primary_key=True)
+    Exe_Questoes_Alternativas = db.Column(db.String(255) , nullable=False)
+ 
+    def __str__(self):
+        return self.name
+
+
+@app.route("/users")
+def users():
+    flash("Template USERS....")
+    return render_template("users.html")
+
+@app.route("/templates")
+def templates():
+    #return "<strong> Hello, World!</strong>"
+    # utilizar render_template()
+    #passando parametros para o template / criar dicionario User
+    users= {
+        "name":"Marcio villela",
+        "idade":54,
+        "email":"mpvillel5@gmail.com"
+    }
+    flash("Template....usuário criado com sucesso!")
+
+    return render_template("templates.html", usersx=users)
+    
 @app.route("/")
 def index():
     users = User.query.all() # Select * from users;
---
+++ aulas curso/aula 52 resources/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, flash
+from datetime import datetime
+from filtros import format_date
+
+app = Flask(__name__,
+template_folder="tema",
+static_folder="public")
+app.config["SECRET_KEY"] = "secret"
+
+app.jinja_env.filters["formatdate"] = format_date
+
+@app.route("/templates")
+def templates():
+    user_page = True
+
+    return render_template("index.html", user_page=user_page)
+
+@app.route("/usuarios")
+def users():
+    users = [{
+        "name": "Marcus Pereira",
+        "age": 99,
+        "email": "oi@spacedevs.com.br",
+        "active": True,
+        "since": datetime.utcnow()
+    },
+    {
+        "name": "amanda silva",
+        "age": 28,
+        "email": "amanda@spacedevs.com.br",
+        "active": False,
+        "since": datetime.utcnow()
+    }]
+    flash(message="Users routes", category="success")
+    return render_template("users.html", users=users)
+
+if __name__ == "__main__":
+    app.run(debug=True)
